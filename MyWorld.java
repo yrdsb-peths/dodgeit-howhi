05164a333b049d17d9af6d29ae1abbf4d05c00d2
howhi <howhiisahop@gmail.com>
2024-04-22 12:00:35 -0400

    Add banana

--- MyWorld.java
+++ MyWorld.java
@@ -19,6 +19,9 @@ public class MyWorld extends World
         super(600, 400, 1); 
         
         Hero hero = new Hero();
-        addObject(hero, 100, 100);        
+        addObject(hero, 100, 100);  
+        
+        Banana banana = new Banana();
+        addObject(banana, 600, 100);
     }
 }
